b1bf8a48789d18dc4ce5d1a4258306d691e752fd
Hirosuke <senolynn7584@gmail.com>
2020-08-30 15:39:25 +0900

    いらねーやつ削除&フォント変更&ぼかし具合変更

--- assets/copy.js
+++
@@ -1,8 +0,0 @@
-function copy(element) {
-  let range = document.createRange();
-  range.selectNodeContents(element);
-  let selection = document.getSelection();
-  selection.removeAllRanges();
-  selection.addRange(range);
-  document.execCommand('copy');
-}
